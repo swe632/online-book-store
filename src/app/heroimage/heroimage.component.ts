b81520328b849104ac5aef894b71ae0d0341f640
deeppp15 <voradeep15@gmail.com>
2023-09-11 23:24:43 -0400

    Resolved NAvbar issue

--- src/app/heroimage/heroimage.component.ts
+++ src/app/heroimage/heroimage.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroimage',
   templateUrl: './heroimage.component.html',
   styleUrls: ['./heroimage.component.css']
 })
-export class HeroimageComponent {
+export class HeroimageComponent implements OnInit{
+  constructor(private router: Router, private route: ActivatedRoute ,private  el: ElementRef) {}
+  ngOnInit() {
+    this.router.events.subscribe(() => {
+      const currentUrl = this.router.url;
+  
+      // Check the current route and apply styles accordingly
+      if (currentUrl.includes('/home')) {
+        console.log("home!!");
+       
+        let normal = this.el.nativeElement.querySelector(".normal"); 
+        let isnormal=normal.classList.contains('normal');
+        if(isnormal){
+          
+          normal.classList.remove('normal');
+          normal.classList.add("hero"); 
+        }
+               //change to hero'
+        let nonmaskmask = this.el.nativeElement.querySelector(".nonmask"); 
+        let isnonmask=nonmaskmask.classList.contains('nonmask');
+        if(isnormal){
+         nonmaskmask.classList.remove('nonmask');
+          nonmaskmask.classList.add("mask"); 
+        }
+
+        let notinto = this.el.nativeElement.querySelector(".notinto"); 
+        let isnotinto=notinto.classList.contains('notinto');
+        if(isnormal){
+          notinto.classList.remove('notinto');
+          notinto.classList.add("into-img"); 
+        }
+
+        let content = this.el.nativeElement.querySelector(".hide"); 
+        let iscontent=content.classList.contains('hide');
+        if(isnormal){
+         content.classList.remove('hide');
+          content.classList.add("content"); 
+        }
+      } 
+    });
+  }
+
 
 }
